db64f3f8071ecb3b36a45da4b1aee2013b96a48d
Ran Shaham <shahamran91@gmail.com>
2017-06-22 19:18:45 +0300

    somewhat working

--- code/1/preprocessing.py
+++ code/1/preprocessing.py
@@ -12,31 +12,35 @@ DATA_FILES = [path.join(DATA_DIR, 'haaretz.csv'),
 CLEANER = re.compile(r'(?:[^a-zA-Z ])+')
 
 
-def clean_data(df):
-    df = df.copy()
+def clean_data(df, inplace=False):
+    df = df.copy() if inplace is False else df
 
     def data_cleaner(title):
         title = title.lower()
         title = CLEANER.sub(' ', title)
         return title
+
     df.loc[:, 'title'] = df.title.apply(data_cleaner)
+    return df
+
+def extract_features(df, inplace=False):
+    df = df.copy() if inplace is False else df
     words = set()
     for title in df.title.values:
         words = words.union(set(title.split()))
     words_dict = {i: word for i, word in enumerate(words)}
-    for i in words_dict:
-        df.loc[:, i] = 0
+    word_to_index = {word: i for i, word in enumerate(words)}
+    df = pd.concat([df, pd.DataFrame(columns=np.arange(len(words)))])
 
     def phi(row):
-        for i in words_dict:
-            if words_dict[i] in row.title:
-                row[i] = 1
-
+        text = row.title
+        for word in text.split():
+            row[word_to_index[word]] = 1
         return row
-    df = df.apply(phi, axis=1)
-    return df, words_dict
+    df = df.apply(phi, axis=1).fillna(0).drop('title', 1)
+    return df, word_to_index
 
 def read_data(filenames=DATA_FILES):
     df, labels = load_dataset(filenames=filenames)
     df = df.rename(columns={0: 'title'})
-    return df, labels
+    return df, np.array(labels)
--- code/1/test.py
+++ code/1/test.py
@@ -1,4 +1,28 @@
 import preprocessing as pre
+import numpy as np
+import sklearn as skl
+from sklearn import svm
 
+# read the data
 df, labels = pre.read_data()
-clean, words = pre.clean_data(df.iloc[:10])
+# shuffle it
+indices = np.random.permutation(len(labels))
+df = df.iloc[indices].reset_index(drop=True)
+labels = labels[indices]
+
+# clean and extract features (word2vec)
+df = pre.clean_data(df.iloc[:1000])
+labels = labels[:1000]
+clean, word_to_index = pre.extract_features(df)
+
+total_length = len(clean)
+alpha = 0.8
+train_thresh = int(total_length * alpha)
+train_data, train_labels = clean.iloc[:train_thresh], labels[:train_thresh]
+test_data, test_labels = clean.iloc[train_thresh:], labels[train_thresh:]
+
+clf = svm.SVC()
+clf.fit(train_data.values, train_labels)
+predictions = clf.predict(test_data.values)
+
+print('accuracy:', sum(test_labels == predictions) / len(predictions))
